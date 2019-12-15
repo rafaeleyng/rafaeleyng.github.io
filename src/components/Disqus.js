e80d42587fada6eadf0624232c13d91fe8a6cc9e
rafaeleyng <rafaeleyng@gmail.com>
2019-12-15 12:46:42 -0300

    fix disqus for new domain

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -7,7 +7,7 @@ var disqus_config = function () {
 
 (function() { // DON'T EDIT BELOW THIS LINE
   var d = document, s = d.createElement('script');
-  s.src = 'https://https-rafaeleyng-github-io.disqus.com/embed.js';
+  s.src = 'https://rafaeleyng.disqus.com/embed.js';
   s.setAttribute('data-timestamp', +new Date());
   (d.head || d.body).appendChild(s);
 })();
